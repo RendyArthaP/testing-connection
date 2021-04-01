8bc7c13b426f81ad20d0d0ffbc029232c89f8520
Rendy Artha <rendyartha312@gmail.com>
2021-04-01 22:37:45 +0700

    mapping belom kelar

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Card, Button, Container, Col, Row } from 'react-bootstrap'
 
-const BookCard = (books) => {
-  const buku = books.books
-  console.log(buku)
+const BookCard = (buku) => {
+  const books = buku.buku
+  console.log(books)
   return (
     <div>
       {/* {buku.map((book) => (
         <Container>
           <Row>
             <Col>
-              <Card style={{ width: '18rem' }}>
+              <Card style={{ width: '18rem' }} key={book.id}>
                 <Card.Img variant="top" />
                 <Card.Body>
                   <Card.Title>{book.namaBuku}</Card.Title>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import { getDataBuku } from '../redux/actions/buku.actions'
 const Home = () => {
   const dispatch = useDispatch()
   const students = useSelector((state) => state.student.data)
-  const books = useSelector((state) => state.buku.data.data)
+  const buku = useSelector((state) => state.buku.data.data)
 
   useEffect(() => {
     dispatch(getDataBuku())
@@ -16,7 +16,7 @@ const Home = () => {
     <div>
       <h1>Hello, {students.name}</h1>
       <h4>List books you need:</h4>
-      <BookCard books = {books}/>
+      <BookCard buku = {buku}/>
     </div>
   )
 }
